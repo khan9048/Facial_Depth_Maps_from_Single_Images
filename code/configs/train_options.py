f24a9db9c2f4272b995188b60d306a2f9a6673f9
Faisal Khan <49758542+khan9048@users.noreply.github.com>
2022-04-11 13:22:18 +0100

    Add files via upload

---
+++ code/configs/train_options.py
@@ -0,0 +1,21 @@
+from configs.base_options import BaseOptions
+
+class TrainOptions(BaseOptions):
+    def initialize(self):
+        parser = BaseOptions.initialize(self)
+
+        # experiment configs
+        parser.add_argument('--epochs',      type=int,   default=30)
+        parser.add_argument('--lr',          type=float, default=1e-4)
+        
+        parser.add_argument('--crop_h',  type=int, default=448)
+        parser.add_argument('--crop_w',  type=int, default=576)        
+        parser.add_argument('--log_dir', type=str, default='./logs')
+
+        # logging options
+        parser.add_argument('--val_freq', type=int, default=1)
+        parser.add_argument('--save_freq', type=int, default=10)
+        parser.add_argument('--save_model', action='store_true', default=True)
+        parser.add_argument('--save_result', action='store_true', default=True)
+
+        return parser
